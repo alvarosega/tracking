6f1b3d7e5f463181fa8e72d62bc7fac034b0987e
alvarosega <segatairon@gmail.com>
2026-09-12 15:02:24 -0400

    correcciones hechas a mgiracion client audits

--- database/migrations/2026_09_07_100002_create_client_audits_table.php
+++ database/migrations/2026_09_07_100002_create_client_audits_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->foreign('client_id')
                 ->references('id_cliente')
                 ->on('reference_clients')
-                ->cascadeOnDelete();
+                ->onDelete('cascade');
         });
     }
 
