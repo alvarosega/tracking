c056f62c56da4d4e90f702737ba3d036e28f3749
alvarosega <segatairon@gmail.com>
2026-09-13 10:44:42 -0400

    coreccines hehcas a la hora???

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,23 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Forzar timezone a nivel de PHP y Carbon
+        date_default_timezone_set('America/La_Paz');
+        Carbon::setLocale('es');
+
+        // Forzar timezone a nivel de la sesión de MySQL
+        DB::statement("SET time_zone = '-04:00'");
     }
 }
