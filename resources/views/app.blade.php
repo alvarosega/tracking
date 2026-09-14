0a911096f3e9f5f0bd77fea8c2c4c6df7ebeff22
alvarosega <segatairon@gmail.com>
2026-09-14 08:08:18 -0400

    correcciones hehcsa en vista

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Panel de Supervisión - Tracking Ventas</title>
+    
+    @routes
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
