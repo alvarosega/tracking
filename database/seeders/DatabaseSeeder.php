1aa1b84617f6927527dcba460878de18c43080ef
alvarosega <segatairon@gmail.com>
2026-09-14 21:04:35 -0400

    chore: agregar seeder de prueba tdb 99 y ajustes de gitignore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             PlanRuteoSeeder::class,
+            TestVendedorSeeder::class,
+            
         ]);
     }
 }
