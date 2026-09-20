e616f286ec118be6b731ea6f0bdc0d02bc34af70
alvarosega <segatairon@gmail.com>
2026-09-20 15:44:59 -0400

    correccionehehca a schedull

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use App\Http\Middleware\CheckActiveWorkday;
 use App\Http\Middleware\EnsureUserIsSupervisor;
 use App\Http\Middleware\HandleInertiaRequests;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -24,6 +25,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'supervisor'     => EnsureUserIsSupervisor::class,
             'workday.active' => CheckActiveWorkday::class,
         ]);
+    })
+    ->withSchedule(function (Schedule $schedule): void {
+        $schedule->command('workday:close-expired')->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Ejecución periódica para cerrar jornadas fuera del horario legal
+// Registrar la tarea programada
 Schedule::command('workday:close-expired')->everyMinute();
